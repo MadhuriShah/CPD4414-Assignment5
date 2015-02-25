d9dcc3961a856d83b416fd82bd40f45298b2f8c7
MadhuriShah <madhurishah393@gmail.com>
2015-02-24 23:09:51 -0600

    delete method

--- src/java/products.java
+++ src/java/products.java
@@ -62,16 +62,15 @@ public class products extends HttpServlet{
         Set<String> keySet = request.getParameterMap().keySet();
         try (PrintWriter out = response.getWriter()) {
             if (keySet.contains("id") && keySet.contains("product_name") && keySet.contains("product_description") && keySet.contains("quantity")) {
-                // There are some parameters     
-                String id = request.getParameter("id");
+                  String id = request.getParameter("id");
                 String name = request.getParameter("product_name");
                 String description = request.getParameter("product_description");
                 String quantity = request.getParameter("quantity");
 
                 doUpdate("INSERT INTO PRODUCT (product_id, product_name, product_description, quantity) VALUES (?, ?, ?, ?)", id, name, description, quantity);
             } else {
-                // There are no parameters at all
-                out.println("Error: Not enough data to input. Please use a URL of the form /servlet?name=XXX&age=XXX");
+    
+                out.println("Not enough data to input");
             }
         } catch (IOException ex) {
             Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
@@ -92,6 +91,37 @@ public class products extends HttpServlet{
         }
         return changes;
     }
+     protected void doPut(HttpServletRequest request, HttpServletResponse response) {
+        Set<String> keySet = request.getParameterMap().keySet();
+        try (PrintWriter out = response.getWriter()) {
+            if (keySet.contains("id") && keySet.contains("product_name") && keySet.contains("product_description") && keySet.contains("quantity")) {
+                
+                String id = request.getParameter("product_id");
+                String name = request.getParameter("product_name");
+                String description = request.getParameter("product_description");
+                String quantity = request.getParameter("quantity");
+                doUpdate("UPDATE PRODUCT SET product_id = ?, product_name = ?, product_description = ?, quantity = ? WHERE PRODUCT_ID = ?", id, name, description, quantity, id);
+            } else {
+                out.println("Not enough data to input");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+      protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
+        Set<String> keySet = request.getParameterMap().keySet();
+        try (PrintWriter out = response.getWriter()) {
+            if (keySet.contains("id")) {               
+                String id = request.getParameter("id");
+                doUpdate("DELETE FROM PRODUCT WHERE product_id = ?", id);
+            } else {
+                out.println("Not enough arguments");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
    
 }
 
